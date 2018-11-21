57cb15461eae9e80d35678f3b75644919c78d183
Kirk Bradley Peterkin <inform@forbiddencolors.com>
2018-11-21 16:53:17 +0100

    Pass the Basic Test Coumn Test

--- todo-kanban/src/App.js
+++ todo-kanban/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TodoApp, TodoList} from './components/TodoList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>
+          (Mini)Kanban board
+        </h1>
+        <div className="kanban--board">
+          <div className="kanban-columns todo" >
+            <TodoApp />
+          </div>
+          <div className="kanban-columns inprogress" />
+          <div className="kanban-columns done" />
+        </div>
       </div>
     );
   }
