3541b3452565f51c6d5aec256f1c5b6581892a57
Kirk Bradley Peterkin <inform@forbiddencolors.com>
2018-11-23 00:32:34 +0100

    Drag and Drop and Editing Work

--- todo-kanban/src/App.js
+++ todo-kanban/src/App.js
@@ -13,12 +13,7 @@ class App extends Component {
           <div className="kanban-columns todo" >
             <TodoApp />
           </div>
-          <div className="kanban-columns inprogress">
-            <TodoApp />
-          </div>
-          <div className="kanban-columns done" >
-            <TodoApp />
-          </div>
+
         </div>
       </div>
     );
--- todo-kanban/src/components/TodoList.js
+++ todo-kanban/src/components/TodoList.js
@@ -1,80 +1,182 @@
 import React, { Component } from 'react';
 import '../styles/TodoList.css';
 
-export class TodoList extends React.Component {
-  render() {
-    let createItem = (item) => {
-      return (
-        <li key={item.id}>
-          {item.text}
-        </li>
-      )
-    };
 
-    return (
-      <ul className="list--tasks">
-        {this.props.items.map(createItem)}
-      </ul>
-    );
-  }
-}
-
-export class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: [
-        {id: 1, text: 'item 1 longer', created_at: '2018-11-19'},
-        {id: 2, text: 'item 2 longer description', created_at: '2016-11-18'}
-      ],
-      text: '',
-      input_empty: true
+export class TodoApp extends Component {
+    state = {
+        tasks: [
+            {name:"Learn Angular",category:"wip", bgcolor: ""},
+            {name:"React", category:"wip", bgcolor:""},
+            {name:"Vue", category:"complete", bgcolor:"skyblue"},
+            {name:"Kanban", category:"backlog", bgcolor:"grey"}
+          ],
+          text: '',
+          input_empty: true,
+          category: ''
     }
-  }
-  onChange = (ev) => {
+
+    onChange = (ev) => {
+
+        console.log('target',ev.target.value, !ev.target.value.trim(), ev.target.attributes['category'].value)
+
     this.setState({
       text: ev.target.value,
-      input_empty: !ev.target.value.trim()
+      input_empty: !ev.target.value.trim(),
+      category: ev.target.attributes['category'].value
     });
   };
-  showAdd = (ev) => {
+  showAdd = (e,cat) => {
+    console.log(cat)
     this.setState({
-      input_empty: false
+      input_empty: false,
+      category: cat,
+      text:''
     });
   };
   handleSubmit = (ev) => {
       ev.preventDefault();
+      console.log(ev)
       if (!this.state.text.trim()) {
         this.setState({input_empty: true});
         return;
-    }
-    
-    var nextItems = this.state.items.concat([{
+        }
+
+        var nextItems = this.state.tasks.concat([{
       id: Date.now(),
-      text: this.state.text,
-      created_at: new Date()
+      name: this.state.text,
+      category: this.state.category,
+      bgcolor: "skyblue"
     }]);
-    this.setState({items: nextItems, text: '', input_empty: true});
+    this.setState({tasks: nextItems, text: '', input_empty: true});
+    console.log('targetvalue:', ev.target)
   };
-  render() {
-    return (
-      <div className='list--container'>
-        <div className="list--header">
-          <div className="list--title">To-Do</div>
-          <div className="list--add" onClick={this.showAdd} ></div>
-        </div>
-        
-        <form onSubmit={this.handleSubmit} className={this.state.input_empty ? "form-inline has-danger" : "form-inline"}>
-          <div className={'form-group'}>
-            <input onChange={this.onChange} value={this.state.text} className='form-control' />
-            {' '}
-          </div>
-        </form>
-        <TodoList items={this.state.items} />
-      </div>
-    );
-  }
+
+
+    onDragStart = (ev, id) => {
+        console.log('dragstart:',id);
+        ev.dataTransfer.setData("id", id);
+    }
+
+    onDragOver = (ev) => {
+        ev.preventDefault();
+    }
+
+    onDrop = (ev, cat) => {
+       let id = ev.dataTransfer.getData("id");
+       
+       let tasks = this.state.tasks.filter((task) => {
+           if (task.name == id) {
+               task.category = cat;
+           }
+           return task;
+       });
+
+       this.setState({
+           ...this.state,
+           tasks
+       });
+
+
+    }
+
+    _handleDoubleClickItem = (event) => {
+        console.log(event.target.name)
+        event.target.disabled=false;
+    }
+
+    render() {
+        var tasks = {
+            backlog: [],
+            wip: [],
+            complete: []
+        }
+
+        this.state.tasks.forEach ((t,i ) => {
+            tasks[t.category].push(
+
+                <div onDragStart = {(e) => this.onDragStart(e, t.name)}
+                    draggable
+                    className="draggable" 
+                    type="text"
+                    key={i}
+                    onDoubleClick={(e) => this._handleDoubleClickItem(e)} 
+                     >
+                     <form onSubmit={this.handleSubmit}>
+                        <input disabled type="text" defaultValue={t.name} onChange={this.onChange} category={t.category} />
+                     </form>
+                     </div>
+            );
+        });
+
+        return (
+            <div className="container-drag">
+
+                <h2 className="header">(Mini)Kanban board</h2>
+            <div className="kanbanBoard">
+                <div className="backlog"
+                    onDragOver={(e)=>this.onDragOver(e)}
+                    onDrop={(e)=>{this.onDrop(e, "backlog")}}>
+                    <div className="list--header">
+                      <div className="list--title">To-Do</div>
+                      <div className="list--add" onClick={(e)=>this.showAdd(e, 'backlog')} ></div>
+                    </div>
+
+                    <div>
+                        <form onSubmit={this.handleSubmit} className={!this.state.input_empty && this.state.category ==='backlog' ? "form-inline add--backlog" : "form-inline"}>
+                          <div className={'form-group'}>
+                            <input onChange={this.onChange} category="backlog" defaultValue='' className='form-control' />
+                            {' '}
+                          </div>
+                        </form>
+                    </div>
+
+                    {tasks.backlog}
+                </div>
+
+                <div className="wip"
+                    onDragOver={(e)=>this.onDragOver(e)}
+                    onDrop={(e)=>{this.onDrop(e, "wip")}}>
+                    <div className="list--header">
+                      <div className="list--title">In-Progress</div>
+                      <div className="list--add" onClick={(e)=>this.showAdd(e, 'wip')} ></div>
+                    </div>
+
+                    <div>
+                        <form onSubmit={this.handleSubmit} className={!this.state.input_empty && this.state.category ==='wip' ? "form-inline add--wip" : "form-inline"}>
+                          <div className={'form-group'}>
+                            <input onChange={this.onChange} category="wip" defaultValue={this.state.text} className='form-control' />
+                            {' '}
+                          </div>
+                        </form>
+                    </div>
+
+                    {tasks.wip}
+                </div>
+                
+                <div className="complete" 
+                    onDragOver={(e)=>this.onDragOver(e)}
+                    onDrop={(e)=>this.onDrop(e, "complete")}>
+                     <div className="list--header">
+                      <div className="list--title">Done</div>
+                      <div className="list--add" onClick={(e)=>this.showAdd(e,'complete')} ></div>
+                    </div>
+
+                    <div>
+                        <form onSubmit={this.handleSubmit} className={!this.state.input_empty  && this.state.category ==='complete'? "form-inline add--complete" : "form-inline"}>
+                          <div className={'form-group'}>
+                            <input onChange={this.onChange} category="complete" defaultValue={this.state.text} className='form-control' />
+                            {' '}
+                          </div>
+                        </form>
+                    </div>
+                     {tasks.complete}
+                </div>
+            </div>
+
+
+            </div>
+        );
+    }
 }
 
 
